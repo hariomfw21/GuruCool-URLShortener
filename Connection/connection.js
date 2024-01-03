4c2e5a8d23c5ec9f9898e6979d0c1e0adaee4e59
hariomfw21 <darkphinixgamer@gmail.com>
2024-01-03 08:44:30 +0530

    Organized project folder structure for improved clarity and efficiency.

--- Connection/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
-
-const connnection = mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-
-module.exports = {
-    connnection
-}
